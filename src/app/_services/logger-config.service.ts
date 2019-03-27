96dbe8ba6eedbf76070a018ab0c47f9f9867b2c8
Vadim <pv@petrovvadim.com>
2019-03-27 23:39:28 +0300

    few fixes

--- src/app/_services/logger-config.service.ts
+++ src/app/_services/logger-config.service.ts
@@ -1,4 +1,4 @@
-import { ILoggerConfig } from "../_interfaces/logger-config.interface";
-import { InjectionToken } from "@angular/core";
+import { ILoggerConfig } from '../_interfaces/logger-config.interface';
+import { InjectionToken } from '@angular/core';
 
 export const LoggerConfigService = new InjectionToken<ILoggerConfig>('LoggerConfig');
